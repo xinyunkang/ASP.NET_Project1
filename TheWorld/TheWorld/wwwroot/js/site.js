810beac29c2105e2ce0dba6d442e460864259c3d
xinyunkang <xinyunkang@gmail.com>
2017-09-06 18:59:37 -0500

    Jquery

--- TheWorld/TheWorld/wwwroot/js/site.js
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -1,3 +1,21 @@
 ﻿//site.js
-var userName = document.getElementById("username");
-userName.innerHTML = "Bayern Kang";
+(function () {
+    var userName = $("#username");
+    userName.text("Bayern Kang");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.style = "backgroundColor : #888;";
+    });
+
+    main.on("mouseleave", function () {
+        main.style = "";
+    });
+
+    var menuItem = $("ul.menu li a");
+    menuItem.on("click", function () {
+        var me = $(this);
+        alert(me.text());
+        
+    })
+})();
