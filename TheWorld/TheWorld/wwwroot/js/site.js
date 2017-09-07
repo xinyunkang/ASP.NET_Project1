d3631d572d15f3d484b8d741145550e90d4b7323
xinyunkang <xinyunkang@gmail.com>
2017-09-06 22:42:36 -0500

    jquery for css hide-show

--- TheWorld/TheWorld/wwwroot/js/site.js
+++ TheWorld/TheWorld/wwwroot/js/site.js
@@ -1,21 +1,32 @@
 ﻿//site.js
 (function () {
-    var userName = $("#username");
-    userName.text("Bayern Kang");
+    //var userName = $("#username");
+    //userName.text("Bayern Kang");
 
-    var main = $("#main");
-    main.on("mouseenter", function () {
-        main.style = "backgroundColor : #888;";
-    });
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "backgroundColor : #888;";
+    //});
 
-    main.on("mouseleave", function () {
-        main.style = "";
-    });
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
 
-    var menuItem = $("ul.menu li a");
-    menuItem.on("click", function () {
-        var me = $(this);
-        alert(me.text());
+    //var menuItem = $("ul.menu li a");
+    //menuItem.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
         
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
     })
+
 })();
