2d1b4b5cb29a3fcfe81f960311365381ccd51c98
xinyunkang <xinyunkang@gmail.com>
2017-09-09 20:58:26 -0500

    api--part1

---
+++ TheWorld/TheWorld/Controllers/Api/TripsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Api
+{
+    [Route("api/trips")]
+    public class TripsController: Controller
+    {
+        private IWorldRepository _repository;
+
+        public TripsController(IWorldRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            
+            return Ok(_repository.GetAllTrips());
+        }
+
+        [HttpPost("")]
+        public IActionResult Post([FromBody]TripViewModel theTrip)
+        {
+            if (ModelState.IsValid)
+            {
+                return Created($"api/trips/{theTrip.Name}", theTrip);
+            }
+            return BadRequest(ModelState);
+        }
+
+    }
+}
