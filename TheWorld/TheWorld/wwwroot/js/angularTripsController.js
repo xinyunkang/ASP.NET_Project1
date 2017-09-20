08ec496e47b2209e8f54d4b32248fbda11fff9b2
xinyunkang <xinyunkang@gmail.com>
2017-09-19 22:09:46 -0500

    Angular table

---
+++ TheWorld/TheWorld/wwwroot/js/angularTripsController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+    //Getting the existing module
+    angular.module("app-trips")
+        .controller("angularTripsController", angularTripsController); //Angular way to create new controllers.
+
+    function angularTripsController() {
+        var vm = this;
+        vm.trips = [
+            {
+                name: "US Trip",
+                created: new Date()
+            },
+            {
+                name: "World Trip",
+                created: new Date()
+            }];
+    }
+
+
+
+})();
---
+++ TheWorld/TheWorld/wwwroot/js/app-trips.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";
+
+    //creating the module
+    angular.module("app-trips", []); //name of the code package, and dependency. [] means creating the module
+
+
+
+})();
