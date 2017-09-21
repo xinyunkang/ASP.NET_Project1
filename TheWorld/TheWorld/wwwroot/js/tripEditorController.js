9cf20d32d8ff44c0c4a9e9e96fe44ff9358d487e
xinyunkang <xinyunkang@gmail.com>
2017-09-21 11:12:11 -0500

    Version 1 Done
    need to improve:
    1.authentication
    2.save / print btn in right upper page

--- TheWorld/TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorld/TheWorld/wwwroot/js/tripEditorController.js
@@ -10,19 +10,57 @@
         vm.isBusy = true;
         vm.errorMessage = "";
         vm.stops = [];
-
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        vm.newStop = {};
+        var url = "/api/trips/" + vm.tripName + "/stops";
+        $http.get(url)
             .then(function (response) {  //then
                 //success
                 angular.copy(response.data, vm.stops); //response.data, NOT stops
-            }, function(err){   //NOT },{
+                _showMap(vm.stops);
+            }, function (err) {   //NOT },{
                 //fail
                 vm.errorMessage = "Fail to load stops";
             })
             .finally(function () {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function () {
+            vm.isBusy = true;
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+                    //succcess
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = [];
+                }, function (err) {
+                    //fail
+                    vm.errorMessage = "Fail to add new stop";
+                }).finally(function () {
+                    vm.isBusy = false;
+                })
+        }
+
     }
 
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+            var mapStops = _.map(stops, function (item) {
+                return {
+                    lat: item.latitude,
+                    long: item.longitude,
+                    info: item.name
+                };
+            });
+
+            //show map
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            })
+        }
+    }
 
 })();
